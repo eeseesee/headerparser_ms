20350f8c8a35677d089d57a395db1787d86956b1
ECC <echristeleit@gmail.com>
2016-02-17 16:09:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Header Parser Microservice
+
+A JavaScript microservice built using Node.js, and Express that fulfills the following user story:
+
+1. User Story: I can get the IP address, language and operating system for my browser.
+
+Built for the Header Parser Microservice API Project at Free Code Camp.
+
+A running version of the app can be found at http://ecc-headerparser-ms.herokuapp.com/
---
+++ app/controllers/serviceHandler.server.js
@@ -0,0 +1,30 @@
+'use strict';
+function ServiceHandler () {
+
+	this.whoami = function (req, res) {
+    // To see the header in its entirety, uncomment the following line
+    //res.send(JSON.stringify(req.headers));
+
+
+    // initialize returns as null
+    var ipaddress = null,
+        language = null,
+        software = null;
+
+    ipaddress = req.ip;
+
+    language = req.headers["accept-language"].split(',')[0];
+
+    var softwareRegEx = /\((.*?)\)/;
+    software = req.headers["user-agent"].match(softwareRegEx)[1];
+
+    res.json({
+        ipaddress: ipaddress,
+        language: language,
+        software: software
+      });
+
+	};
+}
+
+module.exports = ServiceHandler;
